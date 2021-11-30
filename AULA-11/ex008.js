9beb7b0e73b89580e75c5c9a5b1554c0824fc374
Kevin "d415uk3" Wallen <kevinwallen.contato@gmail.com>
2021-11-30 08:50:41 -0300

    ex add/resolved

---
+++ AULA-11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 45
+console.log(`A Velocidade atual é ${velocidade}Km/h`)
+if (velocidade > 60) {
+    console.log(`Você ultrapassou o limite de velocidade, pague R$1`)
+}
+console.log('Feliz Natal')
