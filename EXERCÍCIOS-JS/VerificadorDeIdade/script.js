44a125e98a5529bdba55603566a87dd6ee5dc383
Kevin "d415uk3" Wallen <kevinwallen.contato@gmail.com>
2021-12-05 13:59:35 -0300

    Update script.js

--- EXERCÍCIOS-JS/VerificadorDeIdade/script.js
+++ EXERCÍCIOS-JS/VerificadorDeIdade/script.js
@@ -4,10 +4,41 @@ function verificar() {
     var fAno = document.querySelector("#txtano")
     var res = document.querySelector('div#res')
     
-    if (fAno.value.length == 0 || fAno.value > ano) {
+    if (fAno.value.length == 0 || Number(fAno.value) > ano) {
         window.alert('Verifique os dados inseridos!')
     } else {
-        window.alert('Tudo OK!')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+        var img = document.createElement('img') //Criando tag img
+        img.setAttribute('id','foto') //Criando ID e dando nome
+        if (fsex[0].checked) {
+            genero = 'Masculino'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', './assets/foto-bebe-m.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', './assets/foto-jovem-m.png')                
+            } else if (idade < 50) {
+                img.setAttribute('src', './assets/foto-adulto-m.png')
+            } else {
+                img.setAttribute('src', './assets/foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Feminino'
+            if (idade >=0 && idade < 10) {
+                //KID
+                console.log('KID')
+            } else if (idade < 21) {
+                //Teen
+            } else if (idade < 50) {
+                //Adult
+            } else {
+                //Old
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Você é ${genero} de ${idade} Anos!`
+        res.appendChild(img).style.padding = '15px'
     }
 }
 
