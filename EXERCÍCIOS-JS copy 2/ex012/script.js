101da1de886f983db9a8c637339297ac919a2922
Kevin "d415uk3" Wallen <kevinwallen.contato@gmail.com>
2021-12-05 20:00:21 -0300

    delete

--- EXERCÍCIOS-JS copy 2/ex012/script.js
+++
@@ -1,8 +0,0 @@
-function carregar(){
-    var msg = document.getElementById('msg')
-    //var imagem = document.getElementById('round-photo')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `São ${hora} Horas!`
-
-}
