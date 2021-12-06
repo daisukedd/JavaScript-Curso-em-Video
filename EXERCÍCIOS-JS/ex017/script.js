e551f9fbd58701f2ac376a01537d7da625da1bb5
Kevin "d415uk3" Wallen <kevinwallen.contato@gmail.com>
2021-12-06 10:05:46 -0300

    ex017 - Tabuada - Finalizada

--- EXERCÍCIOS-JS/ex017/script.js
+++ EXERCÍCIOS-JS/ex017/script.js
@@ -1,8 +1,19 @@
-function carregar(){
-    var msg = document.getElementById('msg')
-    //var imagem = document.getElementById('round-photo')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `São ${hora} Horas!`
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('selTab')
+
+    if (num.value.length == 0) {
+        window.alert('Erro! Digite um número.')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = '' //Limpa o Select antes de executrar o while
+        while ( c <= 10) {
+            let item = document.createElement('option') //Criando Elemento dentro do Select - Options seria as selecoes 
+            item.text = `${n} X ${c} = ${n*c}` //Inserindo elementos
+            tab.appendChild(item) //Adicionando ao elemento filho
+            c++
+        }
+    }
 
 }
