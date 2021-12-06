3a198acb793fcb8758b8187f09ea9b5a44ed5abb
Kevin "d415uk3" Wallen <kevinwallen.contato@gmail.com>
2021-12-06 09:26:59 -0300

    ex016 - Contador - Finalizado

--- EXERCÍCIOS-JS/ex016/script.js
+++ EXERCÍCIOS-JS/ex016/script.js
@@ -1,4 +1,34 @@
 function contar() {
-    console.log('Contando')
+    let ini = document.getElementById('txti')   //String
+    let fim = document.getElementById('txtf')   //String
+    let passo = document.getElementById('txtp') //String
+    let res = document.getElementById('res')    //String
 
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { //Validando se não está faltando dados
+        res.innerHTML = `&#128531 Não foi possível efetuar a contagem!<br/>Verfique os dados inseridos.`
+    } else {
+        res.innerHTML = 'Contando: <br>'
+
+        let i = Number(ini.value)   //Convertendo String para Number
+        let f = Number(fim.value)   //Convertendo String para Number
+        let p = Number(passo.value) //Convertendo String para Number
+
+        if (p <= 0) {
+            window.alert('Erro, vou considerar PASSO = 1')
+            p = 1
+        }
+
+        if ( i < f) {
+            //Contagem Crescente
+            for (let c = i ; c <= f ; c+= p ) {
+                res.innerHTML += ` ${c} \u{1F449}` //U+1F449	Emojii Unicode
+            }
+        }else {
+            //Contagem Decrescente
+            for (let c = i ; c >= f ; c -= p ) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
